4b350980bb3001a13cd3e6ac0a089d457ef15a33
courtney <cpestka@umich.edu>
2025-03-30 17:12:41 -0400

    1st

--- lab15.cpp
+++ lab15.cpp
@@ -1,16 +1,77 @@
 #include<iostream>
+#include <limits>
 using namespace std;
 
 // Function prototypes
-void getInfo(int&, int&);
-double computeWays(int, int);
-double factorial(int);
+
+double factorial(int number)
+{
+    if (number == 0)
+    {
+        return 1;
+    }else{
+        return number * factorial(number - 1);
+    }
+}
+
 
 /*******************************************************************
 * getLotteryInfo                                                   *
 * Gets and validates lottery info from the user and places it in   *
 * reference parameters referencing variables in the main function. *
 ********************************************************************/
+
+void getInfo(int&n, int&k)
+{
+    bool error;
+    cout<<"How many balls (1-12) are in the pool to pick from? ";
+    cin>>n;
+    error=cin.fail();
+
+    while (n>12){
+        cout<<"Input Error! There must be between 1 and 12 balls."<< endl;
+        cout << "How many balls (1-12) are in the pool to pick from? ";
+        cin>>n;
+
+    }
+    while (error){
+        cout<< "Input Error! There must be between 1 and 12 balls."<<endl;
+        cout<<"How many balls (1-12 are in the pool to pick from? ";
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cin>>n;
+        error = cin.fail();
+    }
+    cout<<"How many balls (1-"<<n<<") will be drawn? ";
+    cin>>k;
+    error=cin.fail();
+
+    while (k>n){
+        cout<< "Input Error!"<<endl;
+        cout<<"How many balls (1-"<<n<<") will be drawn? ";
+        cin>>k;
+    }
+    while (error){
+        cout<<"Input Error!"<<endl;
+        cout<<"How many balls (1-"<<n<<") will be drawn? ";
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cin>>k;
+        error=cin.fail();
+    }
+}
+double computeWays(int n, int k) {
+
+        double ways;
+    double fn=factorial(n);
+    double fk=factorial(k);
+    double fnk=factorial(n-k);
+
+    ways=(fk*fnk)/(fn);
+
+    return ways;
+}
+
 
 
 
@@ -21,10 +82,7 @@ double factorial(int);
 * The formula for this is     k!(n- k)!                            *
 *                             --------                             *
 *                                 n!                               *
-* Note that the computation is done in a way that does not require *
-* multiplying two factorials together. This is done to prevent any *
-* intermediate result becoming so large that it causes overflow.   *
-********************************************************************/
+
 
 
 /*******************************************************************
